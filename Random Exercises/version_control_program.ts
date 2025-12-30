2f11696ad4b47ff61d8008e2bb2b23e1613abccb
hector <hector.ldz@gmail.com>
2025-12-30 09:58:32 +0100

    add new exercise

---
+++ Random Exercises/version_control_program.ts
@@ -0,0 +1,18 @@
+// Input -> string[]
+// Actions -> switch, save 
+
+// Find the branch that has more files in it:
+
+// Example:
+// - switch branch1;
+// - save file 1;
+// - save file 2;
+// - save file 1;
+// - switch branch2;
+// - switch branch3;
+// - save file 1;
+// - save file 2;
+// - save file 3;
+
+// Output -> Branch 3
+
